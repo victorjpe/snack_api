7a5a9d9a231ede0a8272ba80ce1175c0a3942062
Victor <Victor>
2017-12-06 17:23:39 +0530

    Set port for heroku

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const router = require('./router');
 const app = express();
 const googleSheet = require('./googleSheets');
 
+const port = process.env.PORT || 3000;
+
 //authorize the google docs
 googleSheet.setAuth();
 
@@ -11,4 +13,4 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use('/api', router);
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+app.listen(port, () => console.log('Snack Api is Up and Running'));
